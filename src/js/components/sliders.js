05ed705b692cfe12feff893813de2ebecfe4e670
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2023-05-15 06:10:42 +0300

    home

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,9 +1,11 @@
 const swiper = new Swiper(".swiper", {
+    loop: true,
     slidesPerView: 3,
     spaceBetween: 30,
-    loop: true,
+    // loop: true,
+    // slidePerGroup: 2,
     navigation: {
-        nextEl: ".portfolio-section__next",
         prevEl: ".portfolio-section__prev",
+        nextEl: ".portfolio-section__next",
     },
 });
