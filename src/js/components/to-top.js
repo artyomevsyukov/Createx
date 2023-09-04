5fda0a4d68a9fa57b91d4f846bcc79e887455b19
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2023-09-04 14:44:48 +0300

    added scroll

--- src/js/components/to-top.js
+++ src/js/components/to-top.js
@@ -1,1 +1,21 @@
 const scroll = new SmoothScroll(".to-top");
+const toTop = document.querySelector(".to-top");
+const heroHeight = document.querySelector(".hero").offsetHeight;
+
+const isVisibleToTop = (y = 0) => {
+    if (y >= heroHeight) {
+        toTop.classList.add("to-top--active");
+        console.log("added to-top--active");
+    } else {
+        toTop.classList.remove("to-top--active");
+        console.log("remove");
+    }
+};
+
+isVisibleToTop(window.scrollY);
+
+window.addEventListener("scroll", () => {
+    let y = window.scrollY;
+
+    isVisibleToTop(y);
+});
