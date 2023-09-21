15e00fb6d5c15222df6e3d8179e860922e42d4fd
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2023-09-21 14:38:31 +0300

    Added burger

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,5 +1,29 @@
 const burger = document.querySelector(".burger");
+const menu = document.querySelector(".header__nav");
+const overlay = document.querySelector(".overlay");
+
+const disScroll = () => {
+    let t = window.scrollY;
+    document.body.classList.add("dis-scroll"),
+        (document.body.dataset.position = t),
+        (document.body.style.top = -t + "px");
+};
+const enScroll = () => {
+    let t = parseInt(document.body.dataset.position, 10);
+    (document.body.style.top = "auto"),
+        document.body.classList.remove("dis-scroll"),
+        window.scrollTo({ top: t, left: 0 }),
+        document.body.removeAttribute("data-position");
+};
 
 burger.addEventListener("click", (e) => {
-    e.currentTarget.classList.toggle("burger--active");
+    burger.classList.toggle("burger--active");
+    menu.classList.toggle("header__nav--active");
+    overlay.classList.toggle("overlay--active");
+
+    if (burger.classList.contains("burger--active")) {
+        disScroll();
+    } else {
+        enScroll();
+    }
 });
